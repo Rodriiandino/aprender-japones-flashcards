fbade4d4d6c98e30ca17032ad269a3c437bbe964
RodriiAndino <andinorodrigo03@gmail.com>
2024-07-11 12:33:23 -0300

    Refactor custom preparation and last try sections to use <p> tags for consistency

--- src/components/aside/sections/last-try.tsx
+++ src/components/aside/sections/last-try.tsx
@@ -4,7 +4,7 @@ export default function LastTry() {
   return (
     <TooltipCustom text='Feature in development'>
       <section className='h-full flex flex-col justify-center items-center select-none opacity-60'>
-        Last try
+        <p>Last try</p>
         <small className='text-xs text-center opacity-60'>
           This feature is in development and will be available soon.
         </small>
