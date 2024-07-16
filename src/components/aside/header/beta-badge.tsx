cc239304ce6f994333f9f93100563600a18928c4
RodriiAndino <andinorodrigo03@gmail.com>
2024-07-16 12:05:46 -0300

    Update BetaBadge component styling for consistency

--- src/components/aside/header/beta-badge.tsx
+++ src/components/aside/header/beta-badge.tsx
@@ -4,7 +4,7 @@ export default function BetaBadge({ className }: { className?: string }) {
   return (
     <span
       className={cn(
-        'border-2 border-border rounded-xl text-muted-foreground text-xs font-semibold pr-1 pl-1',
+        'border-2 border-border rounded-xl text-muted-foreground text-[10px] font-semibold px-2 ',
         className
       )}
     >
