2c893ebed779db06e6bcf7a62325180db77a9dc1
RodriiAndino <andinorodrigo03@gmail.com>
2024-07-31 21:56:51 -0300

    Add loading and error handling to AI chat modal

--- src/components/modal/chat/chat-modal-input.tsx
+++ src/components/modal/chat/chat-modal-input.tsx
@@ -1,18 +1,33 @@
 import { Button } from '@/components/ui/button'
 import { Textarea } from '@/components/ui/textarea'
-import { Send } from 'lucide-react'
+import { Send, Square, Repeat } from 'lucide-react'
 
 interface ChatModalInputProps {
   handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void
   input: string
   handleInputChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void
+  stop: () => void
+  isLoading: boolean
+  error: any
+  reload: () => void
 }
 
 export default function ChatModalInput({
   handleSubmit,
   input,
-  handleInputChange
+  handleInputChange,
+  stop,
+  isLoading,
+  error,
+  reload
 }: ChatModalInputProps) {
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault()
+      handleSubmit(e as unknown as React.FormEvent<HTMLFormElement>)
+    }
+  }
+
   return (
     <form
       onSubmit={handleSubmit}
@@ -23,16 +38,43 @@ export default function ChatModalInput({
         className='min-h-10 h-10 text resize-none border-none focus-visible:ring-0 shadow-none'
         value={input}
         onChange={handleInputChange}
+        disabled={isLoading}
+        onKeyDown={handleKeyDown}
       />
-      <Button
-        type='submit'
-        variant={'secondary'}
-        size={'icon'}
-        className='h-10 w-10 rounded-full shadow-none'
-        disabled={!input}
-      >
-        <Send size='20' />
-      </Button>
+
+      {!error && isLoading && (
+        <Button
+          variant='ghost'
+          size='icon'
+          className='h-10 w-10 shadow-none hover:bg-background/70'
+          onClick={stop}
+        >
+          <Square size='20' />
+        </Button>
+      )}
+
+      {!error && !isLoading && (
+        <Button
+          type='submit'
+          variant='ghost'
+          size='icon'
+          className='h-10 w-10 shadow-none hover:bg-background/70'
+          disabled={!input || isLoading}
+        >
+          <Send size='20' />
+        </Button>
+      )}
+
+      {error && (
+        <Button
+          variant='ghost'
+          size='icon'
+          className='h-10 w-10 shadow-none hover:bg-background/70'
+          onClick={reload}
+        >
+          <Repeat size='20' />
+        </Button>
+      )}
     </form>
   )
 }
--- src/components/modal/chat/chat-modal.tsx
+++ src/components/modal/chat/chat-modal.tsx
@@ -12,10 +12,23 @@ import { useModalStore } from '@/store/learn-store'
 import ChatModalInput from './chat-modal-input'
 import ChatModalMessages from './chat-modal-messages'
 import { useChat } from 'ai/react'
+import TooltipCustom from '@/components/tooltip-custom'
+import { Button } from '@/components/ui/button'
+import { Trash } from 'lucide-react'
 
 export default function ChatModal() {
   const { isAiModalOpen, toggleAiModal } = useModalStore()
-  const { messages, input, handleInputChange, handleSubmit } = useChat({
+  const {
+    messages,
+    input,
+    handleInputChange,
+    handleSubmit,
+    isLoading,
+    stop,
+    error,
+    reload,
+    setMessages
+  } = useChat({
     api: '/api/ai/chat'
   })
 
@@ -25,6 +38,18 @@ export default function ChatModal() {
         <DialogHeader>
           <DialogTitle>Chat</DialogTitle>
           <DialogDescription>Chat with AI to learn Japanese</DialogDescription>
+          {messages.length > 0 && (
+            <TooltipCustom text='Clear all messages'>
+              <Button
+                variant={'ghost'}
+                size={'sm'}
+                className='absolute top-6 right-1 opacity-70 hover:bg-transparent hover:opacity-100'
+                onClick={() => setMessages([])}
+              >
+                <Trash size={16} />
+              </Button>
+            </TooltipCustom>
+          )}
         </DialogHeader>
         <ChatModalMessages messages={messages} />
         <DialogFooter>
@@ -32,6 +57,10 @@ export default function ChatModal() {
             input={input}
             handleInputChange={handleInputChange}
             handleSubmit={handleSubmit}
+            stop={stop}
+            isLoading={isLoading}
+            error={error}
+            reload={reload}
           />
         </DialogFooter>
       </DialogContent>
