2dab1066f777f289ffc4b89e80946a6226002145
RodriiAndino <andinorodrigo03@gmail.com>
2025-02-22 15:29:37 -0300

    Fix bug

--- src/components/main/section/loading-cards.tsx
+++ src/components/main/section/loading-cards.tsx
@@ -7,7 +7,7 @@ export default function LoadingCards() {
         'w-full h-full md:flex content-start md:flex-wrap md:gap-3 gap-1 overflow-y-auto overflow-x-hidden grid grid-cols-5 pr-2'
       }
     >
-      {new Array(100).fill(null).map((_, index) => (
+      {new Array(80).fill(null).map((_, index) => (
         <SkeletonCard key={index} />
       ))}
     </section>
--- src/components/main/section/section.tsx
+++ src/components/main/section/section.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useMemo } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { AllCharacters } from '@/data/characters'
 import { useConfigLearnStore, useFavoriteStore } from '@/store/learn-store'
 import { CharacterCard, CharacterDetails } from '@/types/card-type'
@@ -8,24 +8,45 @@ import CardList from './card-list'
 import NoCardsAvailable from './no-cards-available'
 import LoadingCards from './loading-cards'
 
-export default function Section() {
-  const { selectedAlphabet } = useConfigLearnStore()
-  const { favoriteCards } = useFavoriteStore()
+const getHiraganaAndKatakanaCards = (
+  characters: Record<string, CharacterDetails>
+): CharacterCard[] => {
+  return Object.values(characters).flatMap(char => [
+    { character: char, type: 'hiragana' },
+    { character: char, type: 'katakana' }
+  ])
+}
 
-  const configCards = useMemo(() => {
-    if (selectedAlphabet === 'favorite') {
+const getConfigCards = (
+  selectedAlphabet: string,
+  favoriteCards: CharacterCard[]
+): CharacterCard[] | CharacterDetails[] => {
+  switch (selectedAlphabet) {
+    case 'favorite':
       return favoriteCards
-    } else if (selectedAlphabet === 'hiragana+katakana') {
-      return Object.values(AllCharacters).flatMap(char => [
-        { character: char, type: 'hiragana' },
-        { character: char, type: 'katakana' }
-      ])
-    } else {
+    case 'hiragana+katakana':
+      return getHiraganaAndKatakanaCards(AllCharacters)
+    default:
       return Object.values(AllCharacters)
-    }
-  }, [selectedAlphabet, favoriteCards]) as CharacterDetails[] | CharacterCard[]
+  }
+}
 
-  if (!configCards) {
+export default function Section() {
+  const { configCards, setConfigCards, selectedAlphabet } =
+    useConfigLearnStore()
+  const { favoriteCards } = useFavoriteStore()
+  const [loading, setLoading] = useState(true)
+
+  const loadCards = useCallback(() => {
+    setConfigCards(getConfigCards(selectedAlphabet, favoriteCards))
+    setLoading(false)
+  }, [selectedAlphabet, favoriteCards, setConfigCards])
+
+  useEffect(() => {
+    loadCards()
+  }, [loadCards])
+
+  if (loading) {
     return <LoadingCards />
   }
 
