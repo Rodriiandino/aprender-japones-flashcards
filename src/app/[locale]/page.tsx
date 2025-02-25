33820ab5fd97e9f9c4bc3ddc5aa41e545a5ea528
RodriiAndino <andinorodrigo03@gmail.com>
2025-02-25 16:28:06 -0300

    dynamic loading

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,28 @@
+import dynamic from 'next/dynamic'
 import Content from '@/components/content'
-import CardModal from '@/components/modal/card/card-modal'
-import ChatModal from '@/components/modal/chat/chat-modal'
-import ConfirmModal from '@/components/modal/confirm-modal'
-import PrimaryHelpModal from '@/components/modal/help/primary-help-modal'
-import LearningModal from '@/components/modal/learning/learning-modal'
 import { Toaster } from '@/components/ui/sonner'
+
+const LearningModal = dynamic(
+  () => import('@/components/modal/learning/learning-modal'),
+  {
+    ssr: false
+  }
+)
+const ConfirmModal = dynamic(() => import('@/components/modal/confirm-modal'), {
+  ssr: false
+})
+const CardModal = dynamic(() => import('@/components/modal/card/card-modal'), {
+  ssr: false
+})
+const ChatModal = dynamic(() => import('@/components/modal/chat/chat-modal'), {
+  ssr: false
+})
+const PrimaryHelpModal = dynamic(
+  () => import('@/components/modal/help/primary-help-modal'),
+  {
+    ssr: false
+  }
+)
 
 export default function Home() {
   return (
